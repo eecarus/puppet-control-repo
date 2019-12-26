f2944fbe9da9e18b749e607ddbadfe0f3eb9f64d
eecarus <6471940+eecarus@users.noreply.github.com>
2019-12-26 15:33:51 -0500

    checkin

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,7 +7,7 @@ class minecraft (
   }
   file { "${install_dir}/server.jar": 
     ensure => file,
-    source => url,
+    source => $url,
     before => Service["minecraft"]
   }
   package { 'java':
