8f1657d48304560283d7aa19747426c53332ece5
eecarus <6471940+eecarus@users.noreply.github.com>
2019-12-26 15:26:56 -0500

    checkin

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,32 @@
-class minecraft {
-  file { '/opt/minecraft':
+class minecraft (
+    $url = 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar', 
+    $install_dir = '/opt/minecraft') {
+      
+  file { $install_dir:
     ensure => directory,
   }
-  file { '/opt/minecraft/server.jar': 
+  file { "${install_dir}/server.jar": 
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
+    source => url,
+    before => Service["minecraft"]
   }
   package { 'java':
     ensure => present,
   }
-  file { '/opt/minecraft/eula.txt': 
+  file { "${install_dir}/eula.txt": 
     ensure => file,
     content => "eula=true",
   }
-  file { '/etc/systemd/system/minecraft.service': 
+  file { "/etc/systemd/system/minecraft.service": 
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service'
+    # source => 'puppet:///modules/minecraft/minecraft.service'
+    content => epp('minecraft/minecraft.service', { 
+      install_dir = $install_dir
+    })
   }
   service { 'minecraft': 
     ensure => running,
-    enable => true
+    enable => true,
+    require => [Package['java'], File["${install_dir}/eula.txt"], File["/etc/systemd/system/minecraft.service"]]
   }
 }
