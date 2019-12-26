997cd28961ba66d82b9c1a422463fd8d6a60fe89
eecarus <6471940+eecarus@users.noreply.github.com>
2019-12-26 12:42:18 -0500

    follow course

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 node default {
   file { '/root/README':
     ensure => file,
-<<<<<<< HEAD
-=======
     content => 'This is a README',
     owner => 'root',
->>>>>>> 0bf188d2fce7b9d91ec8b071591ed38fa8d78c9c
+  }
+  file { '/root/README':
+    owner => 'root',
   }
 }
